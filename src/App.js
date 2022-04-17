9633105d3cb1395f6525b5bdc9b693b5a237d3e6
srnq <77878502+xzjenny@users.noreply.github.com>
2022-04-17 07:50:28 -0700

    Edited router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
-import React, { useState, useEffect,  } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from 'react';
+import { HashRouter, BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Profile from "./pages/Profile";
 import Main from "./pages/Main";
 import Settings from "./pages/Settings";
 import Login from "./pages/Login";
-import { signOut } from 'firebase/auth';
-import { auth } from "./firebase-config"
 
 function App() {
 
   return (
-    <Router>
+    <HashRouter>
       <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/ecoshower" element={<Login />} />
         <Route path="/stopwatch" element={<Main />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
