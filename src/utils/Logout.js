cc5b25841d174e3410c08cb41c05e956ab6d749d
srnq <77878502+xzjenny@users.noreply.github.com>
2022-04-17 08:14:40 -0700

    Edited router

--- src/utils/Logout.js
+++ src/utils/Logout.js
@@ -11,7 +11,7 @@ const Logout = () => {
     signOut(auth).then(() => {
       localStorage.removeItem("isAuth");
       setIsAuth(false);
-      window.location.pathname = "/ecoshower";
+      window.location.pathname = "/";
     })
   }
   
